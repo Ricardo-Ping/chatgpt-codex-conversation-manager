8cc73cb6d7676bf5e255105584f03e947013f872
Ricardo_PING <1004955884@qq.com>
2026-09-11 02:23:22 +0800

    chore: disable exhaustive-deps rule (intentional ref-guard patterns)

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default tseslint.config(
     rules: {
       "no-undef": "off",
       "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }]
     }
   },
